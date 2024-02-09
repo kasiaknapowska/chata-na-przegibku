140d46cb27cc17c56343360e898ef3036539afbc
kasiaknapowska <k.knapowska@gmail.com>
2024-02-09 14:42:02 +0100

    hamburger menu, sass installation

---
+++ src/components/elements/hamburger/Hamburger.jsx
@@ -0,0 +1,19 @@
+import {hamburger, open} from "./hamburger.module.scss"
+
+const Hamburger = ({onHamburgerClick, isMenuOpen}) => {
+
+  return (
+    <div
+      role="presentation"
+      onClick={() => onHamburgerClick()}
+      className={`${hamburger} ${isMenuOpen && open}`}
+    >
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  )
+}
+
+export default Hamburger
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,19 @@
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+"use client"
+import { useState } from "react";
+import Hamburger from "../elements/hamburger/Hamburger";
+import {navbar} from "./navbar.module.scss"
 
-export default Navbar
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  function onHamburgerClick() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+  return (
+    <div className={navbar}>
+      <div>Logo</div>
+      <Hamburger onHamburgerClick={onHamburgerClick} isMenuOpen={isMenuOpen}/>
+    </div>
+  );
+};
+
+export default Navbar;
