ac90fb31b94c374cc1bcb3ac8fa5a61435654eb3
kasiaknapowska <k.knapowska@gmail.com>
2024-02-29 12:49:31 +0100

    trails - text, styles; links

--- src/app/(subpages)/szlaki/page.jsx
+++ src/app/(subpages)/szlaki/page.jsx
@@ -1,7 +1,8 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.scss";
-import { trails } from "@/data/trails";
+import { przegibekTrails } from "@/data/trails";
+import { links } from "@/data/links";
 import { useState } from "react";
 
 export default function Szlaki() {
@@ -12,7 +13,7 @@ export default function Szlaki() {
     }
     setClicked(i);
   };
-  console.log(clicked);
+
   return (
     <>
       <div className="container">
@@ -26,70 +27,107 @@ export default function Szlaki() {
             szlaków.
           </p>
         </section>
-        <section>
-          <h2>Szlaki turystyczne prowadzące do Chaty na Przegibku:</h2>
 
-          {trails.map((trail) => {
-            return (
-              <div key={trail.id} className={styles.singleTrail}>
-                <h3 className={styles.bold}>{trail.name}</h3>
-                <div className={styles.legend}>
-                  {trail.colors.map((color, i) => {
-                    return (
-                      <div key={i + color}>
-                        <div
-                          className={`${
-                            color === "zielony" ? styles.green : undefined
-                          } ${
-                            color === "niebieski" ? styles.blue : undefined
-                          } ${color === "czarny" ? styles.black : undefined} ${
-                            styles.trail
-                          }`}
-                        ></div>
-                        <p>Szlak {color}</p>
+        {przegibekTrails.map((el, i) => {
+          return (
+            <section key={i}>
+              <h2>{el.title}</h2>
+
+              {el.trails.map((trail) => {
+                return (
+                  <div key={trail.id} className={styles.singleTrail}>
+                    <h3 className={styles.bold}>{trail.name}</h3>
+                    <div className={styles.legend}>
+                      {trail.colors.map((color, i) => {
+                        return (
+                          <div key={i + color}>
+                            <div
+                              className={`${
+                                color === "zielony" ? styles.green : undefined
+                              } ${
+                                color === "niebieski" ? styles.blue : undefined
+                              } ${
+                                color === "czarny" ? styles.black : undefined
+                              } ${styles.trail} 
+                        } ${color === "żółty" ? styles.yellow : undefined} ${
+                                styles.trail
+                              }
+                          ${color === "czerwony" ? styles.red : undefined}`}
+                            ></div>
+                            <p>Szlak {color}</p>
+                          </div>
+                        );
+                      })}
+                      <div className={styles.separator}></div>
+                      <div>
+                        <Image
+                          src="/icon-distance-color.svg"
+                          alt="TV"
+                          width={32}
+                          height={32}
+                          priority
+                        />
+                        <p>{trail.distance} km</p>
                       </div>
-                    );
-                  })}
-                  <div className={styles.separator}></div>
-                  <div>
-                    <Image
-                      src="/icon-distance-color.svg"
-                      alt="TV"
-                      width={32}
-                      height={32}
-                      priority
-                    />
-                    <p>{trail.distance} km</p>
+                      <div className={styles.separator}></div>
+                      <div>
+                        <Image
+                          src="/icon-on-foot-color.svg"
+                          alt="TV"
+                          width={32}
+                          height={32}
+                          priority
+                        />
+                        <p>{trail.onFoot}</p>
+                      </div>
+                      <div className={styles.separator}></div>
+                      <p onClick={() => handleToggle(trail.id)}>
+                        Więcej informacji{" "}
+                        <span
+                          className={
+                            clicked === trail.id ? styles.isShown : null
+                          }
+                        ></span>
+                      </p>
+                    </div>
+                    <div
+                      className={`${styles.wrapper} ${
+                        clicked === trail.id ? styles.isShown : null
+                      }`}
+                    >
+                      <p className={styles.inner}>{trail.info}</p>
+                    </div>
                   </div>
-                  <div className={styles.separator}></div>
-                  <div>
-                    <Image
-                      src="/icon-on-foot-color.svg"
-                      alt="TV"
-                      width={32}
-                      height={32}
-                      priority
-                    />
-                    <p>{trail.onFoot}</p>
-                  </div>
-                  <div className={styles.separator}></div>
-                  <p onClick={() => handleToggle(trail.id)}>
-                    Więcej informacji <span className={clicked === trail.id ? styles.isShown : null}></span>
-                  </p>
-                </div>
-                <div
-                  className={`${styles.wrapper} ${
-                    clicked === trail.id ? styles.isShown : null
-                  }`}
-                >
-                  <p className={styles.inner}>{trail.info}</p>
-                </div>
-              </div>
-            );
-          })}
-        </section>
+                );
+              })}
+            </section>
+          );
+        })}
+
         <section>
-          <h2>Szlaki turystyczne prowadzące do pobliskich schronisk:</h2>
+          <h2>Chcesz zaplanować trasę wycieczki? </h2>
+          <p>
+            Skorzystaj z poniższej mapy i opisów udostępnionych przez turystów.{" "}
+          </p>
+
+          <a
+            href={links.mapaTurystyczna}
+            className="btn"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{marginRight: "16px", minWidth: "230px"}}
+          >
+            Mapa turystyczna
+          </a>
+          <a
+            href={links.szlakiTurystyczne}
+            className="btn"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{minWidth: "230px"}}
+          >
+            Szlaki turystyczne
+          </a>
         </section>
       </div>
     </>
